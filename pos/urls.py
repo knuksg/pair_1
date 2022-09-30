66b5d8f07dfee10dfed0f42d420f25438358d409
knuksg <ksgyo0121@gmail.com>
2022-09-30 13:02:04 +0900

    pjt2

--- pos/urls.py
+++ pos/urls.py
@@ -5,4 +5,6 @@ app_name = "pos"
 
 urlpatterns = [
     path('index/', views.index, name='index' ),
+    path('new/', views.new, name='new' ),
+    path('create/', views.create, name='create' ),
 ]
--- pos/views.py
+++ pos/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Review
 # Create your views here.
 
 def index(request):
-    
-    return render(request, 'pos/index.html')
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'pos/index.html', context)
+
+def new(request):
+
+    return render(request, 'pos/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    Review.objects.create(title=title, content=content)
+    return redirect('pos:new')
