256856391a415684ba9bc1ecb2341a994c159d87
knuksg <ksgyo0121@gmail.com>
2022-09-30 13:47:30 +0900

    pjt2

--- pos/views.py
+++ pos/views.py
@@ -10,14 +10,17 @@ def index(request):
     return render(request, 'pos/index.html', context)
 
 def new(request):
-    
-    return redirect('pos:new')
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'pos/new.html', context)
 
 def create(request):
     title = request.GET.get('title')
     content = request.GET.get('content')
     Review.objects.create(title=title, content=content)
-    return redirect('pos:new')
+    return redirect('pos:index')
 
 def detail(request):
     return redirect('pos:index')
