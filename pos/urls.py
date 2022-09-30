dd7b7d0ca32f0a711754ca4134269e41114f0358
cksthf3211 <cksthf321@gmail.com>
2022-09-30 13:36:55 +0900

    pjt

--- pos/urls.py
+++ pos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('index/', views.index, name='index' ),
     path('new/', views.new, name='new' ),
     path('create/', views.create, name='create' ),
+    path('detail/', views.detail, name='detail' ),
 ]
--- pos/views.py
+++ pos/views.py
@@ -10,11 +10,14 @@ def index(request):
     return render(request, 'pos/index.html', context)
 
 def new(request):
-
-    return render(request, 'pos/new.html')
+    
+    return redirect('pos:new')
 
 def create(request):
     title = request.GET.get('title')
     content = request.GET.get('content')
     Review.objects.create(title=title, content=content)
     return redirect('pos:new')
+
+def detail(request):
+    return redirect('pos:index')
