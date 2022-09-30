5093bdd80d1be7c612a1e45f76c76269974cb842
knuksg <ksgyo0121@gmail.com>
2022-09-30 10:20:23 +0900

    pjt2

--- pair_1/wsgi.py
+++ pair_1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pair project.
+WSGI config for pair_1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pair.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pair_1.settings')
 
 application = get_wsgi_application()
